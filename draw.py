85ac5e4279423311f97280ee664875afb3fff5c8
AaronTanKY <aarontanky@g.ucla.edu>
2025-04-26 11:20:11 -0700

    bounding boxes added

---
+++ draw.py
@@ -0,0 +1,110 @@
+import json
+from PIL import Image, ImageDraw, ImageFont
+from PIL import ImageColor
+from io import BytesIO
+from pathlib import Path
+
+# Create a path object
+IMG_PATH = Path("images") / "caleb.jpg"
+JSON_PATH = Path("caleb.json")
+
+with open(JSON_PATH, 'r', encoding='utf-8') as f:
+    json_output = f.read()
+
+# Load and resize image
+im = Image.open(BytesIO(open(IMG_PATH, "rb").read()))
+im.thumbnail([1024,1024], Image.Resampling.LANCZOS)
+
+def parse_json(json_output: str):
+    # Parsing out the markdown fencing
+    lines = json_output.splitlines()
+    for i, line in enumerate(lines):
+        if line == "```json":
+            json_output = "\n".join(lines[i+1:])  # Remove everything before "```json"
+            json_output = json_output.split("```")[0]  # Remove everything after the closing "```"
+            break  # Exit the loop once "```json" is found
+    return json_output
+
+additional_colors = [colorname for (colorname, colorcode) in ImageColor.colormap.items()]
+
+def plot_bounding_boxes(im, bounding_boxes):
+    """
+    Plots bounding boxes on an image with markers for each a name, using PIL, normalized coordinates, and different colors.
+
+    Args:
+        img_path: The path to the image file.
+        bounding_boxes: A list of bounding boxes containing the name of the object
+         and their positions in normalized [y1 x1 y2 x2] format.
+    """
+
+    # Load the image
+    img = im
+    width, height = img.size
+    print(img.size)
+    # Create a drawing object
+    draw = ImageDraw.Draw(img)
+
+    # Define a list of colors
+    colors = [
+    'red',
+    'green',
+    'blue',
+    'yellow',
+    'orange',
+    'pink',
+    'purple',
+    'brown',
+    'gray',
+    'beige',
+    'turquoise',
+    'cyan',
+    'magenta',
+    'lime',
+    'navy',
+    'maroon',
+    'teal',
+    'olive',
+    'coral',
+    'lavender',
+    'violet',
+    'gold',
+    'silver',
+    ] + additional_colors
+
+    # Parsing out the markdown fencing
+    bounding_boxes = parse_json(bounding_boxes)
+
+    font = ImageFont.load_default()
+
+    # Iterate over the bounding boxes
+    for i, bounding_box in enumerate(json.loads(bounding_boxes)):
+      # Select a color from the list
+      color = colors[i % len(colors)]
+
+      # Convert normalized coordinates to absolute coordinates
+      abs_y1 = int(bounding_box["box_2d"][0]/1000 * height)
+      abs_x1 = int(bounding_box["box_2d"][1]/1000 * width)
+      abs_y2 = int(bounding_box["box_2d"][2]/1000 * height)
+      abs_x2 = int(bounding_box["box_2d"][3]/1000 * width)
+
+      if abs_x1 > abs_x2:
+        abs_x1, abs_x2 = abs_x2, abs_x1
+
+      if abs_y1 > abs_y2:
+        abs_y1, abs_y2 = abs_y2, abs_y1
+
+      # Draw the bounding box
+      draw.rectangle(
+          ((abs_x1, abs_y1), (abs_x2, abs_y2)), outline=color, width=4
+      )
+
+      # Draw the text
+      if "label" in bounding_box:
+        draw.text((abs_x1 + 8, abs_y1 + 6), bounding_box["label"], fill=color, font=font)
+
+    # Display the image
+    img.show()
+
+
+plot_bounding_boxes(im, json_output)
+im
